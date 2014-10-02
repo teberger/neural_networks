c348711fc90d233ff71938a351c62e251d7e1344
teberger <tberge01@cs.unm.edu>
2014-10-02 11:25:50 -0600

    /home/taylor/git/neural_networks/homework4/perceptron.hs autocommit from emacs: Thu Oct  2 11:25:50 2014

--- homework4/perceptron.hs
+++ homework4/perceptron.hs
@@ -4,6 +4,9 @@ type Epoch a b = [Sample a b]
 data Sample a b = Sample { datum :: a,
                            classification :: Enum b}
 
+eta_params :: [Double]
+eta_params = [0:1:(0.5)]
+
 main :: IO ()
 main = return ()
 
