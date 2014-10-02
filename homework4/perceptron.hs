8845ead9aaa90f2d146e0462a14b784f9f644ee6
teberger <tberge01@cs.unm.edu>
2014-10-02 11:03:51 -0600

    /home/taylor/git/neural_networks/homework4/perceptron.hs autocommit from emacs: Thu Oct  2 11:03:51 2014

--- homework4/perceptron.hs
+++ homework4/perceptron.hs
@@ -1,8 +1,14 @@
 module Main where
 
-main :: IO ()
-main = return ()
-
 type Epoch a b = [Sample a b]
 data Sample a b = Sample { datum :: a,
                            classification :: Enum b}
+
+main :: IO ()
+main = return ()
+
+getData :: Sample a b -> a
+getData s = datum s
+
+getClass :: Sample a b -> b
+getClass s = classification s
