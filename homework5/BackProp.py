c8e80928758cef7ba8f9fa9a7fc1f75eca999b1b
tberger <tberge01@cs.unm.edu>
2014-10-20 23:25:50 -0600

    fleshing out the main function, added TODOS

--- homework5/BackProp.py
+++ homework5/BackProp.py
@@ -1,16 +1,30 @@
 __author__ = 'Taylor'
 
+from numpy import *
 from neuron import *
 import sys
-from numpy import *
+
 
 def logistic_function(a, x):
+    """
+    The logistic function as defined in the book
+
+    :param a : the slope parameter of the exponential
+    :param x : the input value to calculate
+
+    :rtype : a floating point value
+    """
     return 1 / (1 + exp(a*x))
 
 if __name__ == '__main__':
     layers = sys.argv[1:]
 
+    #TODO: toy with the number of layers, neurons per layer, random seeding,
+
     network = NeuralNetwork('random_weights', 'full-connectivity')
+
+    phi = logistic_function(1)
+
     for layer_number,neuron_count in enumerate(layers):
         neurons = []
         for i in range(neuron_count):
@@ -18,3 +32,6 @@ if __name__ == '__main__':
 
         network.addLayer(layer_number)
 
+    #TODO: Setup output and input layers
+    #TODO: Read input and map to input layer, error correction
+    #TODO: Output charts, plots, generalization graphs & tables, etc.
